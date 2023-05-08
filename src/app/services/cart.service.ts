377b67b4f24dc65f970b0bcf78f83ac5ee238ed2
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-05-08 00:07:09 -0300

    Cart Items Crud, manipulando itens no carinho

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -20,7 +20,7 @@ export class CartService {
 
     if (this.cartItems.length > 0) {
 
-      existingCartItem = this.cartItems.find( tempCartItem => tempCartItem.id === theCartItem.id);
+      existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
 
       alreadyExistsInCart = (existingCartItem != undefined);
     }
@@ -58,5 +58,24 @@ export class CartService {
     }
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)},
               totalQuantity: ${totalQuantityValue}`);
+  }
+
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(
+      tempCartItem => tempCartItem.id === theCartItem.id
+    );
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
   }
 }
