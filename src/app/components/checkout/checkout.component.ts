c93f280761c3038ac0e201e77029b530ea2ec64e
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-05-09 23:30:34 -0300

    adicionado lista de mes e ano para os cartoes

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { BrunoShopFormService } from 'src/app/services/bruno-shop-form.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit{
+export class CheckoutComponent implements OnInit {
 
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
   checkoutFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  constructor(private formBuilder: FormBuilder, private brunoShopFormService: BrunoShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -45,21 +49,51 @@ export class CheckoutComponent implements OnInit{
         expirationYear: ['']
       })
     })
+
+    const startMonth: number = new Date().getMonth() + 1;
+    this.brunoShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        this.creditCardMonths = data;
+      }
+    );
+
+    this.brunoShopFormService.getCreditCardYears().subscribe(
+      data => {
+        this.creditCardYears = data;
+      }
+    );
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.checkoutFormGroup.get('customer')?.value);
   }
 
-  copyShippingAdressToBillingAddress(event: Event){
+  copyShippingAdressToBillingAddress(event: Event) {
     const ischecked = (<HTMLInputElement>event.target).checked;
-   
+
     if (ischecked) {
       this.checkoutFormGroup.controls['billingAddress']
-        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value); 
-    }else{
+        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
   }
-  
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear)
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }else{
+      startMonth = 1;
+    }
+    this.brunoShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        this.creditCardMonths = data;
+      }
+    );
+  }
 }
