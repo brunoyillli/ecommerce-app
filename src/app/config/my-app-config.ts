eb7e927b72141d69b22dee792e51a15775b95498
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-05-19 16:36:08 -0300

    login/logout usando okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa9jmhjr5NUFmTzE5d7',
+        issuer: 'https://dev-49832458.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile', 'email']
+    }
+}
