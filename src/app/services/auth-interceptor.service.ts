0e4913275a18b3450119012980bccc01779b2c3f
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-06-02 10:10:06 -0300

    order history secure backend

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,30 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Observable, from, lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAcess(request, next));
+  }
+
+  private async handleAcess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = this.oktaAuth.getAccessToken();
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+    return await lastValueFrom(next.handle(request));
+  }
+}
