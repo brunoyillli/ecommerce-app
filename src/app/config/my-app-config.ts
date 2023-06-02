2ce43f12d55ecda570c6d8792c1c2ec6e856c12d
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-06-02 12:46:11 -0300

    comunicacao segura https

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa9jmhjr5NUFmTzE5d7',
         issuer: 'https://dev-49832458.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid','profile', 'email']
     }
 }
